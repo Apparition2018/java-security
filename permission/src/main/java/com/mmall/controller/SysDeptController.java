65cab2e50b76e73c9451848e2de74e58ee3845a1
Apparition2018 <88850180@163.com>
2019-07-10 04:29:42 +0800

    2019-7-10 04:29:42

---
+++ permission/src/main/java/com/mmall/controller/SysDeptController.java
@@ -0,0 +1,51 @@
+package com.mmall.controller;
+
+import com.mmall.common.JsonData;
+import com.mmall.dto.DeptLevelDto;
+import com.mmall.param.DeptParam;
+import com.mmall.service.SysDeptService;
+import com.mmall.service.SysTreeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * SysDeptController
+ *
+ * @author Arsenal
+ * created on 2019/7/10 3:40
+ */
+@Controller
+@RequestMapping("/sys/dept")
+@Slf4j
+public class SysDeptController {
+
+    @Resource
+    private SysDeptService sysDeptService;
+    @Resource
+    private SysTreeService sysTreeService;
+
+    /**
+     * 新增部门
+     */
+    @RequestMapping("/save.json")
+    @ResponseBody
+    public JsonData saveDept(DeptParam param) {
+        sysDeptService.save(param);
+        return JsonData.success();
+    }
+
+    /**
+     * 部门树
+     */
+    @RequestMapping("/tree.json")
+    @ResponseBody
+    public JsonData tree() {
+        List<DeptLevelDto> dtoList = sysTreeService.deptTree();
+        return JsonData.success(dtoList);
+    }
+}
