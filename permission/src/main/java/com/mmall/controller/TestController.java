b2aa2c2ecfabc3a92fc641c243b2b9c48439f5f2
Apparition2018 <88850180@163.com>
2019-07-10 03:31:29 +0800

    2019-7-10 03:31:19

--- permission/src/main/java/com/mmall/controller/TestController.java
+++ permission/src/main/java/com/mmall/controller/TestController.java
@@ -1,18 +1,18 @@
 package com.mmall.controller;
 
+import com.mmall.common.ApplicationContextHelper;
 import com.mmall.common.JsonData;
+import com.mmall.dao.SysAclModuleMapper;
 import com.mmall.exception.ParamException;
 import com.mmall.exception.PermissionException;
+import com.mmall.model.SysAclModule;
 import com.mmall.param.TestVo;
 import com.mmall.util.BeanValidator;
+import com.mmall.util.JsonMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections.MapUtils;
-import org.apache.ibatis.exceptions.PersistenceException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Map;
 
 /**
  * TestController
@@ -30,14 +30,21 @@ public class TestController {
     public JsonData hello() {
         log.info("hello");
 //        throw new RuntimeException("test exception"); // msg: System error
-        throw new PermissionException("test exception"); // msg: test exception
-//        return JsonData.success("hello, permission"); // msg: hello, permission
+//        throw new PermissionException("test exception"); // msg: test exception
+        return JsonData.success("hello, permission"); // msg: hello, permission
     }
+
 
     @RequestMapping("/validate.json")
     @ResponseBody
     public JsonData validate(TestVo vo) throws ParamException {
         log.info("validate");
+        SysAclModuleMapper moduleMapper = ApplicationContextHelper.popBean(SysAclModuleMapper.class);
+        SysAclModule module = null;
+        if (moduleMapper != null) {
+            module = moduleMapper.selectByPrimaryKey(1);
+        }
+        log.info(JsonMapper.obj2String(module));
         BeanValidator.check(vo);
 //        try {
 //            Map<String, String> map = BeanValidator.validateObject(vo);
@@ -51,4 +58,6 @@ public class TestController {
 //        }
         return JsonData.success("test validate");
     }
+
+
 }
